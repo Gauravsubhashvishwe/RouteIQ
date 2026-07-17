6aa93fd4e78f4eccba1108420b8d5dd039ca08a9
Gaurav Vishwe <gauravvishwe@gmail.com>
2026-07-17 10:36:23 +0530

    updated tailwind.config.js for setting theme template path parameters.

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+export default{
+    content: ['./index.html', './src/**/*.{js,jsx}'],
+    theme: {
+        extend :{
+            colors: {
+                ink: '#17202a',
+                ocean: '#0f766e',
+                saffron: '#c2410c',
+                leaf: '#15803d',
+                mist: '#f6f7f9',
+            }
+        }
+    },
+    plugins: []
+};
